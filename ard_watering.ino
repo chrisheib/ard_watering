6aa80d1398736a9d9a55ca4378f8ae28fe6852b2
ChrisHeib <chrisheib@gmx.de>
2020-07-09 17:50:36 +0200

    Display V1

--- ard_watering.ino
+++ ard_watering.ino
@@ -1,3 +1,9 @@
+#include <Wire.h> // Wire Bibliothek einbinden
+#include <LiquidCrystal_I2C.h> // Vorher hinzugefügte LiquidCrystal_I2C Bibliothek einbinden
+LiquidCrystal_I2C lcd(0x27, 16, 2); //Hier wird festgelegt um was für einen Display es sich handelt. In diesem Fall eines mit 16 Zeichen in 2 Zeilen und der HEX-Adresse 0x27. Für ein vierzeiliges I2C-LCD verwendet man den Code "LiquidCrystal_I2C lcd(0x27, 20, 4)" 
+
+//--------------------------------------------
+
 const int ap_sensor1 = A0;
 const int dp_pump1 = 2;
 const unsigned long repeatLimit = (long)1000 * 60 * 30;
@@ -22,6 +28,11 @@ void setup() {
   Serial.println("Hi!");
   //Serial.print(repeatLimit);
 
+  lcd.init(); //Im Setup wird der LCD gestartet 
+  lcd.backlight(); //Hintergru ndbeleuchtung einschalten (lcd.noBacklight(); schaltet die Beleuchtung aus). 
+
+  
+
   // Damit nicht im nullten Tick gearbeitet wird.
   delay(200);
 }
@@ -29,6 +40,12 @@ void setup() {
 //--------------------------------------------
 
 void loop() {
+
+  lcd.setCursor(0, 0);//Hier wird die Position des ersten Zeichens festgelegt. In diesem Fall bedeutet (0,0) das erste Zeichen in der ersten Zeile. 
+  lcd.print("Du bist ein"); 
+  lcd.setCursor(0, 1);// In diesem Fall bedeutet (0,1) das erste Zeichen in der zweiten Zeile. 
+  lcd.print("suesser Baer!!!!!"); 
+  
   // put your main code here, to run repeatedly:
   val = analogRead(ap_sensor1);
   Serial.print("Gelesener Wert:");
